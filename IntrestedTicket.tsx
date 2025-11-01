51b015102aee70bc421d8f004a824a3bb9f8706c
LittleDragon <hongnamyip@gmail.com>
2025-11-02 04:55:13 +0800

    improved intrested ticket thingy

--- IntrestedTicket.tsx
+++ IntrestedTicket.tsx
@@ -20,20 +20,22 @@ function interested() {
 export default function IntrestedTicket(){
     return(
       <>
-      <h1 className={`${cinzel.className} text-4xl md:text-5xl mt-[40vh] text-center overflow-clip`}>Grab the ticket if you want to join us!</h1>
+      <h1 className={`${cinzel.className} text-4xl md:text-6xl mt-[40vh] text-center`}>Grab the ticket if you want to join us!</h1>
       {/* pc view */}
-    <div className="relative w-screen h-[80vh] ml-[10%] mt-[10vh] overflow-clip hidden md:flex">
-      <Image src="/ChristmasProm2025/Ticket1.png" alt="ticket image" width={446} height={647} className="absolute z-10"/>
-      <div>
-    <PeelWrapper className="absolute inset-x-[440px] z-20" width={1558} height={647} drag corner={"TOP_RIGHT"} peelPosition={{x:1300, y:100}} options={{
-      backShadow: true,
-      topShadow: false,
-      backShadowSize: 0.12,
-    }} handleDrag={interested}>
-      <PeelTop><Image src={ticketFront} alt="ticket front" width={1558} height={647} /></PeelTop>
-      <PeelBack><Image src={ticketBack} alt="ticket back" width={1558} height={647}/></PeelBack>
-      <PeelBottom><div className="flex h-full"><h1 className={`${cinzel.className} text-9xl text-center m-auto`}>Thank you for joining us!</h1></div></PeelBottom>
-    </PeelWrapper>
+    <div className="relative w-screen h-[80vh] mx-auto mt-[10vh] overflow-clip hidden md:flex">
+      <div className=" w-[1000ox] mx-auto">
+        <Image src="/ChristmasProm2025/Ticket1.png" alt="ticket image" width={223} height={324} className="absolute z-10 flex-223"/>
+        <div >
+      <PeelWrapper className="absolute inset-x-[220px] z-20 flex-779" width={779} height={324} drag corner={"TOP_RIGHT"} peelPosition={{x: 700, y:200}} options={{
+        backShadow: true,
+        topShadow: false,
+        backShadowSize: 0.12,
+      }} handleDrag={interested}>
+        <PeelTop><Image src={ticketFront} alt="ticket front" width={779} height={324} /></PeelTop>
+        <PeelBack><Image src={ticketBack} alt="ticket back" width={779} height={324}/></PeelBack>
+        <PeelBottom><div className="flex h-full"><h1 className={`${cinzel.className} text-7xl text-center m-auto`}>Thank you for joining us!</h1></div></PeelBottom>
+      </PeelWrapper>
+      </div>
     </div>
 </div>
 {/* Mobile View */}
@@ -41,10 +43,10 @@ export default function IntrestedTicket(){
       <Image src="/ChristmasProm2025/Ticket1.png" alt="ticket image" width={84} height={122} className="absolute"/>
 
     <div>
-    <PeelWrapper className="inset-x-[82px] z-20 touch-none" width={293} height={122} drag corner={"TOP_RIGHT"} options={{
-      backReflection: false,
-      backShadow: false,
-      topShadow: true
+    <PeelWrapper className="inset-x-[82px] z-20 touch-none" width={293} height={122} drag corner={"TOP_RIGHT"} peelPosition={{x: 260, y:50}} options={{
+      backShadow: true,
+        topShadow: false,
+        backShadowSize: 0.12,
     }} handleDrag={interested}>
       <PeelTop><Image src={ticketFront} alt="ticket front" width={293} height={122} /></PeelTop>
       <PeelBack><Image src={ticketBack} alt="ticket back" width={293} height={122} /></PeelBack>
